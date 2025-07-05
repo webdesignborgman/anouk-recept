1d428722a3daab21eb51dc36dd837a703c6e70f8
Alex Borgman <info@webdesignborgman.nl>
2025-07-05 19:37:43 +0200

    changed path in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { HeaderWrapper } from './components/HeaderWrapper';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
+      </head>
       <body className="min-h-screen flex flex-col bg-gray-50">
         <HeaderWrapper />
         <main className="flex-1 p-4">{children}</main>
